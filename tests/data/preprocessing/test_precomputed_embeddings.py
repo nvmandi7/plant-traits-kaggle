37f8a8e4266ecb06c714ea6f743447050af80b2f
nvmandi7 <nmandi7@gmail.com>
2024-04-21 20:29:45 -0500

    test fixtures

---
+++ tests/data/preprocessing/test_precomputed_embeddings.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+
+from src.data.preprocessing.precompute_embeddings import main, SimpleImageDataset   
+
+
+def display_embeddings():
+    embeddings_path = "tests/fixtures/data/processed/planttraits2024/train_embeddings.feather"
+    embeddings_df = pd.read_feather(embeddings_path)
+    print(embeddings_df)
+    return
+
+
+def test_main():
+    data_dir = "tests/fixtures/data/raw/planttraits2024/train_images"
+    model_name = "resnet50"
+    embeddings_path = "tests/fixtures/data/processed/planttraits2024/train_embeddings.feather"
+    main(data_dir, embeddings_path)
+
+    display_embeddings()
+    return
+
